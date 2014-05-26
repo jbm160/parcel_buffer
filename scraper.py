763612b4cf00c5fa1cf3b0576933e2eb295885ac
jbm160 <jbm160@hotmail.com>
2014-05-26 00:06:56 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -36,7 +36,7 @@ import requests
 def queryBuffer(buff):
         qparams = {}
         qparams['f'] = "json"
-        qparams['geometry'] = json.dumps({'geometryType': "esriGeometryPolygon", 'geometries': [buff]})
+        qparams['geometry'] = json.dumps({'geometryType': "esriGeometryPolygon", 'geometry': buff})
         qparams['returnGeometry'] = True;
         qparams['outFields'] = json.dumps(["OBJECTID","STANPAR","OWNER","MAIL_ADDR","MAIL_CITY","MAIL_STATE","MAIL_ZIP","PROP_ADDR","PROP_CITY","PROP_ZIP","LAND_USE","ACREAGE"])
         qparams['outSR'] = 2274
